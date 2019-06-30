139d23f3601e51c3bd561c6fe368ddaa7f118b6c
tpang29 <tpang29@gmail.com>
2019-06-29 20:41:45 -0400

    ternary operator

--- src/demo/VertexComparator.java
+++ src/demo/VertexComparator.java
@@ -7,13 +7,9 @@ public class VertexComparator implements Comparator<Vertex> {
 	@Override
 	public int compare(Vertex o1, Vertex o2) {
 		if (o1.getDistance() == null || o2.getDistance() == null) {
-			if (o1.getDistance() == null && o2.getDistance() != null) {
-				return 1; // o1 is lower priority (higher #)
-			} else {
-				return -1; // o1 is higher priority (lower #)
-			} 
+			// higher priority goes to non-null vertex
+			return (o1.getDistance() == null && o2.getDistance() != null) ? 1 : -1;
 		} else {
-
 			if (o1.getDistance().compareTo(o2.getDistance()) < 0) {		
 				return -1; // o1 is higher priority 
 			} else if (o1.getDistance().compareTo(o2.getDistance()) > 0) {
